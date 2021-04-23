313857483e726c5e0871148664788127cbb57c7c
shahruz <hello@shahr.uz>
2021-04-22 22:35:16 -0700

    adds tz2 tz3 support

--- src/app/features/validators.ts
+++ src/app/features/validators.ts
@@ -2,4 +2,4 @@ export const validateETH = (address: string) =>
   /^(0x){1}[0-9a-fA-F]{40}$/i.test(address);
 
 export const validateTZ = (address: string) =>
-  /^(tz1|KT1)[0-9a-z]{33}$/i.test(address);
+  /^(tz|KT)[0-9a-z]{34}$/i.test(address);
